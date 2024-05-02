591f42ce3f80a4b2ceccbbe46e9d93d2f22a6290
ValentinoGonzalezPautaso2 <gonzalezpautasovalentino@gmail.com>
2024-05-02 18:42:24 +0100

    feat: main.c

--- actividades_lkm/ej_01/main.c
+++ actividades_lkm/ej_01/main.c
@@ -7,7 +7,7 @@
 */
 static int __init ej01_module_init(void) {
 	// Completar
-
+	printk("contreras_abril_ej01: Hola desde el kernel");
 	// Salio todo bien
 	return 0;
 }
@@ -17,7 +17,7 @@ static int __init ej01_module_init(void) {
 */
 static void __exit ej01_module_exit(void) {
 	// Completar
-
+	printk("contreras_abril_ej01: Chau desde el kernel!");
 }
 
 // Registro funciones de inicializacion y salida
